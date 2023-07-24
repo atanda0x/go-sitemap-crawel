128919e9a0326eaa7af17dce19ccd3a399b75715
atanda0x <atandanafiu21@gmail.com>
2023-07-23 17:48:54 -0700

    added user agents

--- main.go
+++ main.go
@@ -19,7 +19,14 @@ type parser interface {
 type DefaultParser struct {
 }
 
-//nuserAgents
+var userAgents = []string {
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+}
 
 func randomAgent() {
 
@@ -29,13 +36,15 @@ func extractSiteMapURLs(startURL string) []string {
 	Worklist := make(chan []string)
 	toCrawl := []string{}
 
+	var n int
+	n++
 	go func(link string) { worklist <- []string{startURL} }()
 
 	for ; n > 0; n-- {
-	}
 
 	list := <-Worklist
 	for _, link := range list {
+		n++
 		go func() {
 			response, err := makeRequest(link)
 			if err != nil {
